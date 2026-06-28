479d23969c84b15e6dbf0fd824b948a89c732b99
Musammil N <mohammedmusammiln@gmail.com>
2026-06-28 22:32:03 +0530

    Add clipping modifier to StatusScreens.kt

--- app/src/main/java/com/mntgxo/ytdownloader/ui/screens/StatusScreens.kt
+++ app/src/main/java/com/mntgxo/ytdownloader/ui/screens/StatusScreens.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.mntgxo.ytdownloader.ui.theme.AccentGreen
